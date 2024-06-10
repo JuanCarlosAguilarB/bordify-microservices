9d93e5b6d2cf9fbb5ed092b5f411aed901f54388
JuanC <abjuancarlos.12@gmail.com>
2024-06-10 02:11:48 -0500

    chore: update model User of auth-server.

--- auth-server/src/main/java/com/authserver/models/User.java
+++ auth-server/src/main/java/com/authserver/models/User.java
@@ -1,33 +1,85 @@
 package com.authserver.models;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalTime;
 import java.util.UUID;
 
-@Table(name="\"user\"")
-@AllArgsConstructor
-@Builder
-@NoArgsConstructor
+/**
+ * Represents a user entity in the Bordify application.
+ * This class defines the structure of a user entity, including their username, password, email,
+ * name, verification status, phone number, and timestamps for creation and last login.
+ *
+ * The `User` class leverages Lombok annotations to reduce boilerplate code, generating getters,
+ * setters, constructors, and a builder pattern. Additionally, the `@Table` annotation is used
+ * to specify the database table name as `"\"user\""`, which ensures compatibility with databases
+ * where `user` is a reserved keyword or requires case-sensitive handling.
+ */
 @Entity
 @Data
+@Builder
+@Table(name = "\"user\"")
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
+    /**
+     * Unique identifier for the User. It is automatically generated and uses UUID as the ID type.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID userId;
+    private UUID id;
 
+    /**
+     * Username of the user. This field must be unique.
+     */
+    @Column(unique = true)
     private String userName;
-    private String password;
-    private Boolean isVerified;
 
+    /**
+     * Password of the user.
+     */
+    private String password;
+
+    /**
+     * Email address of the user.
+     */
     private String email;
+
+    /**
+     * First name of the user.
+     */
     private String firstName;
+
+    /**
+     * Last name of the user.
+     */
     private String lastName;
-    private String phone;
+
+    /**
+     * Flag indicating whether the user is verified or not.
+     */
+    private Boolean isVerified = false;
+
+    /**
+     * Phone number of the user.
+     */
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    /**
+     * Time when the user was created.
+     */
+    private LocalTime created;
+
+    /**
+     * Time of the last login of the user.
+     */
+    @Column(name = "last_login")
+    private LocalTime lastLogin;
 
 }
